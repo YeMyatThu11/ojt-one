faee53354c033e0c5c5a442c78389e48c6f4b42f
MyatThu88 <myat.thu.iu88@gmail.com>
2022-02-02 11:04:57 +0630

    fix login controller

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'user' => \App\Http\Middleware\User::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
